e5fc823662070c2ab51d112ca8887eeee9e25dd3
Andrew Lewis <ajlewis912@gmail.com>
2023-11-24 23:29:59 -0800

    Completed Overlay of phot and spec data

--- Project_2.py
+++ Project_2.py
@@ -7,9 +7,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 from astropy.visualization import make_lupton_rgb
 from matplotlib.colors import LogNorm 
+from astroquery.vizier import Vizier
+import astropy.units as u
+from astropy.coordinates import SkyCoord
 
 
-#Call a function to scale the data 
+
+
+#Call a function to scale the data, using the .5 and 99.5 as in DS9 so that the image is much bighter.  
+
+
 
 def scale_data(data): 
 	p_low, p_high = np.percentile(data, [.5, 99.5])
@@ -27,7 +34,6 @@ blue =  fits.open('h_udf_wfc_b_drz_img.fits')
 
 
 
-
 red_data = red[0].data
 blue_data = blue[0].data
 green_data = green[0].data
@@ -40,6 +46,7 @@ blue_scaled = scale_data(blue_data)
 
 
 RGB = make_lupton_rgb(red_scaled, green_scaled, blue_scaled, stretch = 7, Q= 4.5)
+
 
 wcs = WCS(red[0].header)
 
@@ -58,9 +65,48 @@ ax.set_xlabel("RA")
 ax.set_ylabel("DEC")
 
 ax.grid(color = 'white', ls = 'dashed', lw = .1)
-
 plt.show()
 
+#Ok so now we have the color graph now we need to do the second part of the assignemnt. 
+
+
+phot_table = fits.open('asu.fit')
+phot_data = phot_table[1].data
+
+
+phot_coords = SkyCoord(ra = phot_data['RAJ2000'], dec = phot_data['DEJ2000'], unit = (u.deg, u.deg))
+
+x1,y1 = wcs.world_to_pixel(phot_coords)
+ax.scatter(x1,y1,s=5,marker='x',color = 'red', alpha=0.1, label = "Photmetric RedShift")
+
+spec_table = fits.open('asus.fit')
+spec_data = spec_table[1].data
+spec_coords = SkyCoord(ra = spec_data['RAJ2000'], dec = spec_data['DEJ2000'], unit = (u.deg, u.deg))
+
+
+x2,y2 = wcs.world_to_pixel(spec_coords)
+
+ax.scatter(x2, y2, s=5, marker = 'x', color = 'green', alpha = 1, label = "spectrooscopic redshif")
+
+ax.legend()
+plt.show()
+
+#We now have the spec and phot redshifts represented in the HuDF images.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
 
 
 
